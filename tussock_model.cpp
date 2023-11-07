aecce019310e07c4bb4337510bb107fc1fda3d1f
wes <milkmanmahler1017@gmail.com>
2023-11-07 15:39:49 -0500

    improved collision detection and age

--- tussock_model.cpp
+++ tussock_model.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
+#include <map>
 #include "tussock_model.h"
 
 double calculateDistance(const Tiller& tiller) {
@@ -9,16 +10,20 @@ double calculateDistance(const Tiller& tiller) {
 }
 
 void resolveOverlaps(std::vector<Tiller>& tillers) {
-    bool overlaps = true;
-    while (overlaps) {
-        overlaps = false;
-        for (size_t i = 0; i < tillers.size(); ++i) {
-            for (size_t j = i + 1; j < tillers.size(); ++j) {
-                if (tillers[i].isOverlapping(tillers[j])) {
-                    overlaps = true;
-                    // Move the old Tiller
-                    tillers[i].move();
-                }
+    for (size_t i = 0; i < tillers.size(); ++i) {
+        for (size_t j = i + 1; j < tillers.size(); ++j) {
+            // Calculate the distance between the two Tiller objects.
+            double distance = std::sqrt(
+                std::pow(tillers[i].getX() - tillers[j].getX(), 2) +
+                std::pow(tillers[i].getY() - tillers[j].getY(), 2) +
+                std::pow(tillers[i].getZ() - tillers[j].getZ(), 2)
+            );
+
+            double sumOfRadii = tillers[i].getRadius() + tillers[j].getRadius();
+
+            if (distance <= sumOfRadii && distance <= 2) {
+               
+                tillers[i].move();
             }
         }
     }
@@ -34,9 +39,8 @@ int main() {
     // boolean status, dead or alive
 
     //both below matrices dervied from Jim McGraw's data
-    double class_transition_matrix[9][9] = { 
-        {0.0000,0.0063,0.0782,0.1730,0.2739,0.3202,0.4054,0.3779,0.3776},      //probability of a tiller of size class n transitioning to a class m
-        {0.3716,0.4259,0.2133,0.1066,0.0579,0.0367,0.0270,0.0305,0.0459},      //n and m are indices on the matrix
+    double class_transition_matrix[8][9] = {                                //probability of a tiller of size class n transitioning to a class m
+        {0.3716,0.4259,0.2133,0.1066,0.0579,0.0367,0.0270,0.0305,0.0459},   //n and m are indices on the matrix
         {0.1789,0.1230,0.2678,0.1588,0.0802,0.0446,0.0154,0.0076,0.0204},
         {0.1468,0.0599,0.1991,0.2417,0.1470,0.0971,0.0541,0.0267,0.0102},
         {0.1239,0.0189,0.1303,0.2109,0.2272,0.1286,0.0888,0.0496,0.0102},
@@ -50,10 +54,14 @@ int main() {
         {1.0000,0.6467,0.8839,0.8981,0.8753,0.9003,0.9073,0.8969,0.9541}    //probability of a tiller size class n surviving the time step
     };
 
-    std::ofstream outputFile("tiller_data.csv", std::ios::app);  // Open CSV file in append mode
-    outputFile << "TimeStep,SizeClass,Radius,X,Y,Z,Status\n";
+    double tillering_matrix[1][9] = {
+        {0.0000,0.0063,0.0782,0.1730,0.2739,0.3202,0.4054,0.3779,0.3776}   //probability of tiller classes producing a daughter tiller
+    };
 
-    Tiller initial_tiller(1, 0.5, 0.001, 0, 0, 1); // initalize the first tiller at coords 0,0,0,
+    std::ofstream outputFile("tiller_data.csv", std::ios::app);  // Open CSV file in append mode
+    outputFile << "TimeStep,Age,SizeClass,Radius,X,Y,Z,Status\n";
+
+    Tiller initial_tiller(1,1, 0.5, 0.001, 0, 0, 1); // initalize the first tiller at coords 0,0,0,
     std::vector<Tiller> tillers;
     tillers.push_back(initial_tiller);
 
@@ -71,13 +79,23 @@ int main() {
 
                 double distance = calculateDistance(tiller);
                 int size_class = tiller.getSizeClass();
+                int age = tiller.getAge();
                 double surviveEvent = static_cast<double>(std::rand()) / RAND_MAX;
 
-            //first determine if tiller lives or dies
-            if (surviveEvent < survival_matrix[0][size_class-1] / (0.5*distance)) {  //if tiller lives, determine new size class from transition probabilities
-            //for now just divide the survival matrix probabilities by the distance from the center
-            //will need to get actual data to validate that this is good enough
-            //also to see what kind of relationship between distance and survival there is (linear, exponentional, etc)
+                //first determine if tiller lives or dies
+                if (surviveEvent < (survival_matrix[0][size_class-1] / (0.1*distance))) {  //if tiller lives, determine new size class from transition probabilities
+                //for now just divide the survival matrix probabilities by the distance from the center
+                //will need to get actual data to validate that this is good enough
+                //also to see what kind of relationship between distance and survival there is (linear, exponentional, etc)
+ 
+                double tillerEvent = static_cast<double>(std::rand())/ RAND_MAX;
+
+                if (tillerEvent < tillering_matrix[0][size_class-1]) {
+                    Tiller newTiller = tiller.makeDaughter();
+                    resolveOverlaps(tillers);
+                    newTillers.push_back(newTiller); //store new tiller separately, add into total data at the end of iterating through every current tiller
+
+                }
 
                 double transition_prob = static_cast<double>(std::rand()) / RAND_MAX;
 
@@ -93,22 +111,25 @@ int main() {
                     }
                 }
 
-                //will need to relate size class to reproduction
-                //for now just slap in reproduction into every tiller to run and test the model
-                Tiller newTiller = tiller.makeDaughter();
-                resolveOverlaps(tillers);
-                newTillers.push_back(newTiller); //store new tiller separately, add into total data at the end of iterating through every current tiller
+                tiller.growRadius(0.05);
+                tiller.mature(1);
 
             //if it didnt survive, it died:
             } else {
                 tiller.setStatus(0);
+
             };
 
-            step_data.emplace_back(tiller.getSizeClass(), tiller.getRadius(), tiller.getX(), tiller.getY(), tiller.getZ(), tiller.getStatus());
+                step_data.emplace_back(tiller.getAge(), tiller.getSizeClass(), tiller.getRadius(), tiller.getX(), tiller.getY(), tiller.getZ(), tiller.getStatus());
             } 
             else { //now iterate through dead tillers
-                resolveOverlaps(tillers);
-                step_data.emplace_back(tiller.getSizeClass(), tiller.getRadius(), tiller.getX(), tiller.getY(), tiller.getZ(), tiller.getStatus());
+
+                //simulate decay, only add tillers into the data which are not decayed
+                tiller.growRadius(-0.025);
+                if (tiller.getRadius() >= 0.01) {
+                    step_data.emplace_back(tiller.getAge(), tiller.getSizeClass(), tiller.getRadius(), tiller.getX(), tiller.getY(), tiller.getZ(), tiller.getStatus());
+                }
+
             }
         }
 
@@ -116,7 +137,7 @@ int main() {
         tillers.insert(tillers.end(), newTillers.begin(), newTillers.end());
 
         for (Tiller& data : step_data) {
-            outputFile << time_step << "," << data.getSizeClass() <<','<<data.getRadius() << ',' <<data.getX()<<','<<data.getY()<<','<<data.getZ() << ','<< data.getStatus()<<'\n';
+            outputFile << time_step << ',' << data.getAge() << ',' << data.getSizeClass() <<','<<data.getRadius() << ',' <<data.getX()<<','<<data.getY()<<','<<data.getZ() << "," <<data.getStatus()<<'\n';
         }
     }
 
--- tussock_model.h
+++ tussock_model.h
@@ -6,7 +6,7 @@
 
 class Tiller {
     public:
-        Tiller (int size_class, double radius, double x, double y, double z, bool status) : size_class(size_class), radius(radius), x(x), y(y), z(z), status(status) {}
+        Tiller (int age, int size_class, double radius, double x, double y, double z, bool status) : age(age), size_class(size_class), radius(radius), x(x), y(y), z(z), status(status) {}
 
         int getSizeClass() const {return size_class;}
 
@@ -17,6 +17,8 @@ class Tiller {
         double getZ() const {return z;}
 
         bool getStatus() const {return status;}
+
+        int getAge() const {return age;}
 
         void growRadius(double dRadius){
             radius += dRadius;
@@ -30,13 +32,17 @@ class Tiller {
             size_class = new_class;
         }
 
+        void mature (int age_growth) {
+            age += age_growth;
+        }
+
         bool isOverlapping(const Tiller& other) const {
             double distance = std::sqrt(std::pow(x - other.getX(), 2) + std::pow(y - other.getY(), 2) + std::pow(z - other.getZ(), 2));
-            double sumOfRadii = getRadius() + other.getRadius(); // Assuming Tiller has a getRadius() method
+            double sumOfRadii = getRadius() + other.getRadius();
 
             return (distance <= sumOfRadii);
         }
-        void move() {
+        void move(){
 
             std::srand(static_cast<double>(std::rand())); 
 
@@ -55,17 +61,19 @@ class Tiller {
             double xOffset = randomRadius * std::cos(randomAngle);
             double yOffset = randomRadius * std::sin(randomAngle);
 
-            double zOffset = 0.5 * static_cast<double>(std::rand()) / RAND_MAX;
+            //0.1 is to ensure that the daughter tiller is made slightly above the parent
+            double zOffset = 0.1 * static_cast<double>(std::rand()) / RAND_MAX;
 
             double newX = x + xOffset;
             double newY = y + yOffset;
             double newZ = z + zOffset;
 
-            return Tiller(1, 0.5, newX, newY, newZ, 1);
+            return Tiller(1,1, 0.5, newX, newY, newZ, 1);
 
         }
 
     private:
+        int age;
         int size_class; // size class as defined by Jim McGraw
         double radius, x, y, z; // radius of tiller, x,y,z coords
         bool status; // 1 for alive, 0 for dead
