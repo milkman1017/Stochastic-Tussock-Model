99615a56d60be9c7570ebed2500c9f85fbfc39ad
wes <milkmanmahler1017@gmail.com>
2023-11-07 01:23:56 -0500

    added in transition matricies

--- tussock_model.cpp
+++ tussock_model.cpp
@@ -25,23 +25,38 @@ void resolveOverlaps(std::vector<Tiller>& tillers) {
 }
 
 int main() {
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
     //the "tiller" object contains the following:
-    // tiller radius, leaf length, root length (assumed to be 0.5, 20, and 100)
+    // tiller size class, 
+    // radius (assumed to be 0.5)
     // x,y,z coordinates
-    // quaternion x,y,z,w
     // boolean status, dead or alive
 
-    std::ofstream outputFile("tiller_data.csv", std::ios::app);  // Open CSV file in append mode
-    outputFile << "TimeStep,Radius,LeafLength,RootLength,Volume,X,Y,Z\n";
+    //both below matrices dervied from Jim McGraw's data
+    double class_transition_matrix[9][9] = { 
+        {0.0000,0.0063,0.0782,0.1730,0.2739,0.3202,0.4054,0.3779,0.3776},      //probability of a tiller of size class n transitioning to a class m
+        {0.3716,0.4259,0.2133,0.1066,0.0579,0.0367,0.0270,0.0305,0.0459},      //n and m are indices on the matrix
+        {0.1789,0.1230,0.2678,0.1588,0.0802,0.0446,0.0154,0.0076,0.0204},
+        {0.1468,0.0599,0.1991,0.2417,0.1470,0.0971,0.0541,0.0267,0.0102},
+        {0.1239,0.0189,0.1303,0.2109,0.2272,0.1286,0.0888,0.0496,0.0102},
+        {0.1009,0.0095,0.0616,0.1043,0.1670,0.2441,0.1506,0.1031,0.0612},
+        {0.0367,0.0095,0.0118,0.0474,0.1225,0.1837,0.2124,0.1412,0.0969},
+        {0.0367,0.0000,0.0000,0.0284,0.0601,0.1312,0.2124,0.2710,0.2092},
+        {0.0046,0.0000,0.0000,0.0000,0.0134,0.0341,0.1467,0.2672,0.5000},
+    };
 
-    Tiller initial_tiller(0.5,15,100,0.001,0,0,0,0,0,1,1); // initalize the first tiller at coords 0,0,0,0, and parallel with the z axis 
+    double survival_matrix[1][9] = {
+        {1.0000,0.6467,0.8839,0.8981,0.8753,0.9003,0.9073,0.8969,0.9541}    //probability of a tiller size class n surviving the time step
+    };
+
+    std::ofstream outputFile("tiller_data.csv", std::ios::app);  // Open CSV file in append mode
+    outputFile << "TimeStep,SizeClass,Radius,X,Y,Z,Status\n";
+
+    Tiller initial_tiller(1, 0.5, 0.001, 0, 0, 1); // initalize the first tiller at coords 0,0,0,
     std::vector<Tiller> tillers;
     tillers.push_back(initial_tiller);
 
-    //define sim parameters
-    double kr = 200; //constant for creating a daughter tiller
-    double kd = 1; //constant for a tiller dying
-    double kg = 100; //constant for a tiller growing 
 
     int sim_time = 100; //total length of the sim in years
 
@@ -53,25 +68,47 @@ int main() {
 
         for (Tiller& tiller : tillers) {
             if (tiller.getStatus() == 1) { // Check if the tiller is alive
+
                 double distance = calculateDistance(tiller);
-                double totalProb = kr * distance + kd * distance * distance + kg;
-                double reproProb = (kr / distance) / totalProb;
-                double dieProb = (kd * distance * distance) / totalProb;
-                double growProb = kg / totalProb;
+                int size_class = tiller.getSizeClass();
+                double surviveEvent = static_cast<double>(std::rand()) / RAND_MAX;
 
-                double eventProb = static_cast<double>(std::rand()) / RAND_MAX;
+            //first determine if tiller lives or dies
+            if (surviveEvent < survival_matrix[0][size_class-1] / (0.5*distance)) {  //if tiller lives, determine new size class from transition probabilities
+            //for now just divide the survival matrix probabilities by the distance from the center
+            //will need to get actual data to validate that this is good enough
+            //also to see what kind of relationship between distance and survival there is (linear, exponentional, etc)
 
-                if (eventProb < reproProb) {  // Reproducing
-                    Tiller newTiller = tiller.makeDaughter();
-                    resolveOverlaps(tillers);
-                    newTillers.push_back(newTiller); // Store new tiller separately
-                } else if (eventProb < (reproProb + dieProb)) {  // Dying
-                    tiller.setStatus(false);
-                } else {  // Growing
-                    tiller.setRadius(0.001);
+                double transition_prob = static_cast<double>(std::rand()) / RAND_MAX;
+
+                double cumulative_prob = 0.0;
+                int new_size_class = 0;
+
+                for (int m = 0; m < 9; m++) {
+                    cumulative_prob += class_transition_matrix[size_class - 1][m];
+                    if (transition_prob < cumulative_prob) {
+                        new_size_class = m + 1; // Adding 1 to convert from 0-based index to 1-based size class
+                        tiller.setClass(new_size_class);
+                        break;
+                    }
                 }
 
-                step_data.emplace_back(tiller.getRadius(), tiller.getLeafLength(), tiller.getRootLength(), tiller.getX(), tiller.getY(), tiller.getZ(), tiller.getQuatX(), tiller.getQuatY(), tiller.getQuatZ(), tiller.getQuatW(), tiller.getStatus());
+                //will need to relate size class to reproduction
+                //for now just slap in reproduction into every tiller to run and test the model
+                Tiller newTiller = tiller.makeDaughter();
+                resolveOverlaps(tillers);
+                newTillers.push_back(newTiller); //store new tiller separately, add into total data at the end of iterating through every current tiller
+
+            //if it didnt survive, it died:
+            } else {
+                tiller.setStatus(0);
+            };
+
+            step_data.emplace_back(tiller.getSizeClass(), tiller.getRadius(), tiller.getX(), tiller.getY(), tiller.getZ(), tiller.getStatus());
+            } 
+            else { //now iterate through dead tillers
+                resolveOverlaps(tillers);
+                step_data.emplace_back(tiller.getSizeClass(), tiller.getRadius(), tiller.getX(), tiller.getY(), tiller.getZ(), tiller.getStatus());
             }
         }
 
@@ -79,7 +116,7 @@ int main() {
         tillers.insert(tillers.end(), newTillers.begin(), newTillers.end());
 
         for (Tiller& data : step_data) {
-            outputFile << time_step << "," << data.getRadius() <<','<<data.getLeafLength()<<','<<data.getRootLength()<< ',' << data.volume() << ',' <<data.getX()<<','<<data.getY()<<','<<data.getZ() << '\n';
+            outputFile << time_step << "," << data.getSizeClass() <<','<<data.getRadius() << ',' <<data.getX()<<','<<data.getY()<<','<<data.getZ() << ','<< data.getStatus()<<'\n';
         }
     }
 
--- tussock_model.h
+++ tussock_model.h
@@ -6,46 +6,28 @@
 
 class Tiller {
     public:
-        Tiller (double radius, double leaf_length, double root_length, double x, double y, double z, double quat_x, double quat_y, double quat_z, double quat_w, bool status) : radius(radius), leaf_length(leaf_length), root_length(root_length), x(x), y(y), z(z), status(status) {}
+        Tiller (int size_class, double radius, double x, double y, double z, bool status) : size_class(size_class), radius(radius), x(x), y(y), z(z), status(status) {}
+
+        int getSizeClass() const {return size_class;}
 
         double getRadius() const {return radius;}
-        double getLeafLength() const {return leaf_length;}
-        double getRootLength() const {return root_length;}
 
         double getX() const {return x;}
         double getY() const {return y;}
         double getZ() const {return z;}
 
-        double getQuatX() const {return quat_x;}
-        double getQuatY() const {return quat_y;}
-        double getQuatZ() const {return quat_z;}
-        double getQuatW() const {return quat_w;}
-
         bool getStatus() const {return status;}
 
-        double volume() {  // volume is approximated by assuming the root and leaf are cones
-                         // the leaf extends straight out from the angle of the tiller
-                         // the root extends stauight down along the z axis no matter the angle of the tiller
-            double root_vol = (1.0 / 3.0) * 3.14 * root_length * radius * radius;
-            double leaf_vol = (1.0 / 3.0) * 3.14 * leaf_length * radius * radius;
-            return root_vol + leaf_vol;
-
-        }
-
-        void setRadius(double dRadius){
+        void growRadius(double dRadius){
             radius += dRadius;
-        }
-
-        void setLeaf(double dLeaf){
-            leaf_length += dLeaf;
-        }
-
-        void setRoot(double dRoot){
-            root_length += dRoot;
         }
 
         void setStatus(bool new_status) {
             status = new_status;
+        }
+
+        void setClass(int new_class) {
+            size_class = new_class;
         }
 
         bool isOverlapping(const Tiller& other) const {
@@ -79,11 +61,12 @@ class Tiller {
             double newY = y + yOffset;
             double newZ = z + zOffset;
 
-            return Tiller(0.5, 15, 100, newX, newY, newZ, 0, 0, 0, 1, true);
+            return Tiller(1, 0.5, newX, newY, newZ, 1);
 
         }
 
     private:
-        double radius, leaf_length, root_length, x, y, z, quat_x, quat_y, quat_z, quat_w; // radius of tiller, length of leaf, length of root, x,y,z coords, quaternion representation of the angle of the tillers
+        int size_class; // size class as defined by Jim McGraw
+        double radius, x, y, z; // radius of tiller, x,y,z coords
         bool status; // 1 for alive, 0 for dead
 };
