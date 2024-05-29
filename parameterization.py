47ec36ba05b1508879df517729d026a373cdc364
milkman1017 <milkmanmahler1017@gmail.com>
2024-05-29 15:42:35 -0400

    imporved parameterization

--- parameterization.py
+++ parameterization.py
@@ -8,10 +8,8 @@ import configparser
 import random
 import csv
 from PIL import Image
-import seaborn as sns
 from scipy.interpolate import interp1d
-
-#Run $ wget  "https://docs.google.com/spreadsheets/d/1GfVrWWKMBeOzuNMu31YC-pTHkpYPN9guSKM_pvvei5U/export?format=csv&edit#gid=0" -O "parameterization_data.csv" to get most recent data
+import logging
 
 def get_config():
     config = configparser.ConfigParser()
@@ -19,98 +17,70 @@ def get_config():
     return config
 
 def graph_real_data():
-
-    df = pd.read_csv('./tussock_allometry.csv')
-
+    df = pd.read_csv('./tussock_density_tussock_diam.csv')
     scatter_plot = sns.scatterplot(x='Estimated_Age', y='Diameter', hue='Location', data=df, palette='viridis')
-
     plt.xlabel('Estimated Age')
     plt.ylabel('Diameter')
     plt.title('Scatter Plot with Nonlinear Trend Line (All Locations)')
-
     plt.legend(title='Location')
-
     plt.show()
 
 def tussock_model(config):
     makefile_path = 'Makefile'
     make_result = subprocess.run(['make', '-f', makefile_path])
-
     num_sims = int(config.get('Tussock Model', 'nsims'))
     outdir = config.get('Tussock Model', 'filepath')
     num_threads = int(config.get('Tussock Model','nthreads'))
     sim_time = int(config.get('Tussock Model', 'nyears'))
-
-
     if make_result.returncode == 0:
-
         cpp_input = f"{sim_time}\n{num_sims}\n{outdir}\n{num_threads}\n"
-
         process = subprocess.Popen('./tussock_model', stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
         output, _ = process.communicate(input=cpp_input)
-
     else:
         print("Makefile execution failed")
 
 def clean_data(sim_data, messy_data, config):
     sim_length = int(config.get('Tussock Model', 'nyears'))
-
     last_time_step = sim_data['TimeStep'].max()
     last_time_step_rows = sim_data[sim_data['TimeStep'] == last_time_step]
-
     if messy_data != []:
         messy_last = messy_data[-1]
-    else: 
+    else:
         messy_last = 0
-
     if any(last_time_step_rows['Status'] == 1):
         while len(messy_data) < sim_length + 1:
             messy_data.append(messy_last * 10)
     else:
         while len(messy_data) < sim_length + 1:
             messy_data.append(0)
-
     return messy_data
 
 def diameter_objective(config, iteration):
-    training_data = pd.read_csv('./tussock_allometry.csv')
-
+    training_data = pd.read_csv('./tussock_density_tussock_diam.csv')
     num_sims = int(config.get('Tussock Model', 'nsims'))
     sim_filepath = config.get('Tussock Model', 'filepath')
-
-    training_data['field_davg'] = pd.to_numeric(training_data['field_davg'], errors='coerce')
+    training_data['field_davg'] = pd.to_numeric(training_data['diam'], errors='coerce')
     training_data['field_davg'] = training_data['field_davg'].astype(float)
-
     training_diameters = training_data['field_davg'].values
-
     training_diameters = training_diameters[~np.isnan(training_diameters)]
-
     obv_hist, obv_bins = np.histogram(training_diameters, bins='auto', density=True)
-
     sim_diameters = []
 
     for i in range(num_sims):
         sim_data = pd.read_csv(f'{sim_filepath}/tiller_data_sim_num_{i}.csv')
         last_time_step_data = sim_data[sim_data['TimeStep'] == sim_data['TimeStep'].max()]
-
         num_alive_tillers = last_time_step_data['Status'].sum()
         num_dead_tillers = (last_time_step_data['Status'] == 0).sum()
 
-        if num_alive_tillers >= 2*num_dead_tillers:
-            diameter = 60
-        else:
-            diameter = abs(sim_data[sim_data['TimeStep'] == sim_data['TimeStep'].max()]['X'].max() - sim_data[sim_data['TimeStep'] == sim_data['TimeStep'].max()]['X'].min())
-
-        if diameter == 0.0:
-            diameter=1
-            sim_diameters.append(diameter)
-        else:
-            sim_diameters.append(diameter)
+        diameter = abs(sim_data[sim_data['TimeStep'] == sim_data['TimeStep'].max()]['X'].max() - sim_data[sim_data['TimeStep'] == sim_data['TimeStep'].max()]['X'].min())
         
+        if diameter == 0.0:
+            diameter=0.1
+
+        sim_diameters.append(diameter)
+
     model_hist, model_bins = np.histogram(sim_diameters, bins=obv_bins, density=True)
-
     rmse = np.sqrt(np.mean((obv_hist - model_hist)**2))
-
     output_dir = 'mean_diameter_frames'
     os.makedirs(output_dir, exist_ok=True)
     sns.kdeplot(training_diameters, label='Observed Tussock Diameter Distribution', linewidth=1)
@@ -118,135 +88,106 @@ def diameter_objective(config, iteration):
     plt.legend()
     plt.title(f'Training Iteration: {iteration}')
     plt.xlabel('Tussock Diameter')
-
     frame_filename = os.path.join(output_dir, f'Mean_Tuss_diameter_iteration_{iteration}.png')
     plt.savefig(frame_filename)
     plt.close()
-
     del training_data
     return rmse
 
-def calculate_parameters(parameters, config, iteration, previous_loss, dloss):
+def calculate_parameters(parameters, config, iteration, previous_loss, dloss, log_file, opt_iteration):
     optimization_data = pd.read_csv('./optimization_results.csv')
     gradients = dict()
 
-    if dloss ==0.0:
-        learning_rate = previous_loss
-    else:
-        learning_rate = previous_loss
-
     learning_rate = 0.1
-
-    print('learning rate: ', learning_rate)
-    print('-------')
-    for key in parameters:
-        print('Parameter: ', key)
-        gradient = np.gradient(optimization_data['loss'], optimization_data[key])
-        if gradient[-1] == 0.0:
-            parameters[key] = parameters[key]
-        else:
-            print('gradient: ', gradient[-1])
-            norm_gradient = gradient / np.linalg.norm(gradient, ord=2)
-            print('norm_gradient: ', norm_gradient[-1])
-            parameter_change = learning_rate * norm_gradient[-1] 
-            print('unclipped parameter change: ', parameter_change)
-            parameter_change = np.clip(parameter_change, -0.1, 0.1)
-            print('clipped parameter change: ', parameter_change)
-            parameters[key] = parameters[key] - parameter_change
-            print('==============================')
-
+    
+    with open(log_file, 'a') as log:
+        log.write(f'Iteration: {iteration}\n')
+        log.write(f'Learning rate: {learning_rate} \n')
+        for key in parameters:
+            gradient = np.gradient(optimization_data['loss'], optimization_data[key])
+            if gradient[-1] == 0.0:
+                parameters[key] = parameters[key]
+            else:
+                norm_gradient = gradient / np.linalg.norm(gradient, ord=2)
+                parameter_change = learning_rate * gradient[-1]
+                parameter_change = np.clip(parameter_change, -0.5, 0.5)
+                parameters[key] = parameters[key] - parameter_change
+            log.write(f'Parameter: {key}, Change: {parameter_change}, Gradient: {gradient[-1]}, Normalized Gradient: {norm_gradient[-1]} \n')
     return gradients
 
 def write_parameters(parameters, config):
     outdir = config.get('Parameterization','outdir')
-
-    #save parameters to use in sim
     with open(f'{outdir}/parameters.txt', 'w') as file:
         for param_name, param_value in parameters.items():
             file.write(f"{param_name}={param_value}\n")
 
 def write_optimization_results(parameters, loss, iteration, config):
     outdir = config.get('Parameterization','outdir')
-
-    # save parameters and loss for analysis
     csv_filepath = f'{outdir}/optimization_results.csv'
+    param_history_filepath = f'{outdir}/parameter_history.csv'
+
     mode = 'w' if iteration == 0 else 'a'
 
     with open(csv_filepath, mode, newline='') as csvfile:
         fieldnames = list(parameters.keys()) + ['loss']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        # Write header only in the first iteration
         if iteration == 0:
             writer.writeheader()
-
         writer.writerow({**parameters, 'loss': loss})
 
 def animate_fitting(outdir, filename, iteration, outfilename):
     frames = []
-
     for timestep in range(iteration):
         frame_filename = os.path.join(outdir, f'{filename}_{timestep}.png')
         frames.append(Image.open(frame_filename))
-
-    frames[0].save(outfilename, save_all=True, append_images=frames[1:], duration=50, loop=0)
-
+    frames[0].save(outfilename, save_all=True, append_images=frames[1:], duration=75, loop=0)
     for frame_filename in os.listdir(outdir):
         os.remove(os.path.join(outdir, frame_filename))
     os.rmdir(outdir)
 
 def main():
     config = get_config()
-
     parameters = dict()
-
-    # graph_real_data()
+    log_file = 'parameter_updates.log'
+    with open(log_file, 'w') as log:
+        log.write('Parameter Updates Log\n')
 
     opt_iteration = 0
-    dloss=100
-
+    dloss = 100
     while opt_iteration <= 1:
-        print('------')
-        print('Iteration: ', opt_iteration)
-
-        #initialize random variables first
         parameters['ks'] = np.random.uniform(0.3, 0.6)
         parameters['kr'] = random.uniform(0.3, 0.6)
         
+        for i in range(8):
+            for j in range(9):
+                parameters[f'class_transition_matrix_{i}_{j}'] = np.random.uniform(0.0001,1.0)
+
+        for i in range(9):
+            parameters[f'survival_matrix_{i}'] = np.random.uniform(0.0001,1.0)
+            parameters[f'tillering_matrix_{i}'] = np.random.uniform(0.0001,1.0)
+
         write_parameters(parameters, config)
-
         tussock_model(config)
-        
-        #is this loss
         loss = diameter_objective(config, opt_iteration)
-
+        with open(log_file, 'a') as log:
+            log.write(f'Iteration: {opt_iteration}, Loss: {loss}\n')
         write_optimization_results(parameters, loss, opt_iteration, config)
-
         opt_iteration += 1
 
-    print('dloss: ', dloss, ' loss: ', loss)
-    while opt_iteration  < 150 and loss > 0.03 and dloss >0.0001:
-        print('-----')
-        print('dloss: ', dloss, ' loss: ', loss)
-        print('Iteration: ', opt_iteration)
-
-        gradients = calculate_parameters(parameters, config, opt_iteration, loss, dloss)
+    while opt_iteration < 100 and loss > 0.015 and dloss > 0.0001:
+        gradients = calculate_parameters(parameters, config, opt_iteration, loss, dloss, log_file, opt_iteration)
         write_parameters(parameters, config)
-
         tussock_model(config)
-
         previous_loss = loss
         loss = diameter_objective(config, opt_iteration)
-
         dloss = abs(previous_loss - loss)
-
+        with open(log_file, 'a') as log:
+            print(loss, dloss)
+            log.write(f'Iteration: {opt_iteration}, Loss: {loss}, dLoss: {dloss}\n')
         write_optimization_results(parameters, loss, opt_iteration, config)
-
-        opt_iteration+=1
+        opt_iteration += 1
 
     animate_fitting('mean_diameter_frames', f'Mean_Tuss_diameter_iteration', opt_iteration, 'diameter_dist_fitting.gif')
-    
-    # animate_fitting('num_tillers_frames', f'num_tillers_iteration', opt_iteration, 'num_tiller_fitting.gif')
 
 if __name__ == '__main__':
     main()
--- tussock_model.cpp
+++ tussock_model.cpp
@@ -7,14 +7,12 @@
 #include <filesystem>
 #include <random>
 
-
 double calculater0(const Tiller& tiller) {
     return std::sqrt(tiller.getX() * tiller.getX() + tiller.getY() * tiller.getY());
 }
 
-double calculateTillersDistance(const Tiller& tiller1, const Tiller& tiller2){
+double calculateTillersDistance(const Tiller& tiller1, const Tiller& tiller2) {
     return std::sqrt((tiller1.getX() - tiller2.getX()) * (tiller1.getX() - tiller2.getX()) + ((tiller1.getY() - tiller2.getY()) * (tiller1.getY() - tiller2.getY())));
-
 }
 
 void resolveOverlaps(std::vector<Tiller>& tillers, std::random_device& rd) {
@@ -24,7 +22,6 @@ void resolveOverlaps(std::vector<Tiller>& tillers, std::random_device& rd) {
         overlapsExist = false;  // Assume no overlaps initially
         for (size_t i = 0; i < tillers.size(); ++i) {
             for (size_t j = i + 1; j < tillers.size(); ++j) {
-               
                 if (calculateTillersDistance(tillers[i], tillers[j]) < 3.5 && tillers[i].isOverlapping(tillers[j])) {
                     overlapsExist = true;
 
@@ -35,32 +32,46 @@ void resolveOverlaps(std::vector<Tiller>& tillers, std::random_device& rd) {
 
                     tillers[i].move(angle, overlappingDistance);
                     tillers[j].move(angle + M_PI, overlappingDistance);
-
                 }
             }
         }
     }
 }
 
-void readFromFile(const std::string& filename, double& ks, double& kr) {
+void readFromFile(const std::string& filename, double& ks, double& kr, double class_transition_matrix[8][9], double survival_matrix[9], double tillering_matrix[9]) {
     std::ifstream inputFile(filename);
 
     if (inputFile.is_open()) {
-        // Assuming the file format is "ks=value" and "kr=value etc"
         inputFile.ignore(256, '=');
         inputFile >> ks;
 
         inputFile.ignore(256, '=');
         inputFile >> kr;
+
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                inputFile.ignore(256, '=');
+                inputFile >> class_transition_matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < 9; ++i) {
+            inputFile.ignore(256, '=');
+            inputFile >> survival_matrix[i];
+        }
+
+        for (int i = 0; i < 9; ++i) {
+            inputFile.ignore(256, '=');
+            inputFile >> tillering_matrix[i];
+        }
         
         inputFile.close();
-
     } else {
         std::cerr << "Unable to open file: " << filename << std::endl;
     }
 }
 
-void input(int &sim_time, int &num_sims, std::string &outdir, long unsigned int &num_threads){
+void input(int &sim_time, int &num_sims, std::string &outdir, long unsigned int &num_threads) {
     std::cout << "Enter Simulation time in Years: ";
     std::cin >> sim_time;
 
@@ -75,83 +86,56 @@ void input(int &sim_time, int &num_sims, std::string &outdir, long unsigned int 
     std::cin >> num_threads;
 }
 
-void simulate(const int max_sim_time, const int sim_id, const std::string outdir) {
+void simulate(const int max_sim_time, const int sim_id, const std::string& outdir) {
     double ks, kr;
-    readFromFile("parameters.txt", ks, kr);
+    double class_transition_matrix[8][9];
+    double survival_matrix[9];
+    double tillering_matrix[9];
+
+    readFromFile("parameters.txt", ks, kr, class_transition_matrix, survival_matrix, tillering_matrix);
 
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dis(0.0, 1.0);
-    std::uniform_real_distribution<double> time(40, max_sim_time);
+    std::uniform_real_distribution<double> time(1, max_sim_time);
 
     int sim_time = time(gen);
 
     std::cout << sim_time << "\n";
 
-    //the "tiller" object contains the following:
-    // tiller size class, 
-    // radius (assumed to be 0.5)
-    // x,y,z coordinates
-    // boolean status, dead or alive
-
-    //both below matrices dervied from Jim McGraw's data
-    double class_transition_matrix[8][9] = {                                //probability of a tiller of size class n transitioning to a class m
-        {0.3716,0.4259,0.2133,0.1066,0.0579,0.0367,0.0270,0.0305,0.0459},   //n and m are indices on the matrix
-        {0.1789,0.1230,0.2678,0.1588,0.0802,0.0446,0.0154,0.0076,0.0204},
-        {0.1468,0.0599,0.1991,0.2417,0.1470,0.0971,0.0541,0.0267,0.0102},
-        {0.1239,0.0189,0.1303,0.2109,0.2272,0.1286,0.0888,0.0496,0.0102},
-        {0.1009,0.0095,0.0616,0.1043,0.1670,0.2441,0.1506,0.1031,0.0612},
-        {0.0367,0.0095,0.0118,0.0474,0.1225,0.1837,0.2124,0.1412,0.0969},
-        {0.0367,0.0000,0.0000,0.0284,0.0601,0.1312,0.2124,0.2710,0.2092},
-        {0.0046,0.0000,0.0000,0.0000,0.0134,0.0341,0.1467,0.2672,0.5000},
-    };
-
-    double survival_matrix[9] = {
-        1.0000,0.6467,0.8839,0.8981,0.8753,0.9003,0.9073,0.8969,0.9541    //probability of a tiller size class n surviving the time step
-    };
-
-    double tillering_matrix[9] = {
-        0.0000,0.0063,0.0782,0.1730,0.2739,0.3202,0.4054,0.3779,0.3776   //probability of tiller classes producing a daughter tiller
-    };
-
     std::string out_file_name = outdir + "/tiller_data_sim_num_" + std::to_string(sim_id) + ".csv";
     std::ofstream outputFile(out_file_name, std::ios::ate);  // Open CSV file in append mode
 
-    Tiller first_tiller(1,1, 0.5, 0.001, 0, 0, 3, 1); // initalize the first tiller at coords 0,0,0,...attributes are age, size class, radius, x,y,z, num fo roots, and status
+    Tiller first_tiller(1, 1, 0.5, 0.001, 0, 0, 3, 1); // initialize the first tiller at coords 0,0,0
     std::vector<Tiller> previous_step;
     previous_step.push_back(first_tiller);
 
     std::vector<std::string> buffer;
+    std::ofstream logFile("too_many_alive_tillers.log", std::ios::app);
 
     for (int time_step = 0; time_step <= sim_time; time_step++) {
         std::vector<Tiller> step_data;
         std::vector<Tiller> newTillers; // Store new tillers separately
-
-        // std::cout << "Time Step: " << time_step <<" Number of Tillers: " << previous_step.size() << '\n';
 
         int alive_tillers = 0;
 
         for (Tiller& tiller : previous_step) {
             if (tiller.getStatus() == 1) { // Check if the tiller is alive
 
-                alive_tillers +=1;
+                alive_tillers += 1;
 
                 double distance = calculater0(tiller);
                 int size_class = tiller.getSizeClass();
                 double surviveEvent = dis(gen);
 
-                //first determine if tiller lives or dies
-                if (surviveEvent < (survival_matrix[size_class-1] / (ks*distance))) {  //if tiller lives, determine new size class from transition probabilities
-                    //for now just divide the survival matrix probabilities by the distance from the center
-                    //will need to get actual data to validate that this is good enough
-                    //also to see what kind of relationship between distance and survival there is (linear, exponentional, etc)
-    
+                // first determine if tiller lives or dies
+                if (surviveEvent < (survival_matrix[size_class - 1] / (ks * distance))) { // if tiller lives, determine new size class from transition probabilities
+
                     double tillerEvent = dis(gen);
 
-                    if (tillerEvent < (tillering_matrix[size_class-1] / (kr*distance))) {
+                    if (tillerEvent < (tillering_matrix[size_class - 1] / (kr * distance))) {
                         Tiller newTiller = tiller.makeDaughter();
-                        newTillers.push_back(newTiller); //store new tiller separately, add into total data at the end of iterating through every current tiller
-                        // resolveOverlaps(newTillers)
+                        newTillers.push_back(newTiller); // store new tiller separately, add into total data at the end of iterating through every current tiller
                     }
 
                     double transition_prob = dis(gen);
@@ -169,29 +153,25 @@ void simulate(const int max_sim_time, const int sim_id, const std::string outdir
                     }
 
                     tiller.growRadius(0.01);
-                    tiller.mature(1);  //increase age by one year, this will also increase the stem base by 2 mm
+                    tiller.mature(1);  // increase age by one year, this will also increase the stem base by 2 mm
 
-                    std::uniform_int_distribution<int> int_dis(2,4);
+                    std::uniform_int_distribution<int> int_dis(2, 4);
                     int new_roots = int_dis(gen);
                     tiller.growRoots(new_roots);
 
-            //if it didnt survive, it died:
-            } else {
-                tiller.setStatus(0);
-
-            };
+                } else { // if it didn't survive, it died:
+                    tiller.setStatus(0);
+                }
 
                 step_data.emplace_back(tiller.getAge(), tiller.getSizeClass(), tiller.getRadius(), tiller.getX(), tiller.getY(), tiller.getZ(), tiller.getNumRoots(), tiller.getStatus());
-            } 
-            else { //now iterate through dead tillers
+            } else { // now iterate through dead tillers
 
-                //simulate decay, only add tillers into the data which are not decayed for now decay is estimates at -10% biomass per year, lots of conflcting info, will need to do more lit review
+                // simulate decay, only add tillers into the data which are not decayed
                 tiller.decay();
                 tiller.growRoots(0);
                 if (tiller.getRadius() >= 0.01) {
                     step_data.emplace_back(tiller.getAge(), tiller.getSizeClass(), tiller.getRadius(), tiller.getX(), tiller.getY(), tiller.getZ(), tiller.getNumRoots(), tiller.getStatus());
                 }
-
             }
         }
 
@@ -201,24 +181,30 @@ void simulate(const int max_sim_time, const int sim_id, const std::string outdir
         resolveOverlaps(step_data, rd);
 
         for (Tiller& data : step_data) {
-            buffer.emplace_back(std::to_string(time_step) + ',' + std::to_string(data.getAge()) + ',' + std::to_string(data.getSizeClass()) + ','+ std::to_string(data.getRadius()) + ',' + std::to_string(data.getX()) + ',' + std::to_string(data.getY()) + ',' + std::to_string(data.getZ()) + "," + std::to_string(data.getNumRoots()) + "," + std::to_string(data.getStatus()) + '\n');
+            buffer.emplace_back(std::to_string(time_step) + ',' + std::to_string(data.getAge()) + ',' + std::to_string(data.getSizeClass()) + ',' + std::to_string(data.getRadius()) + ',' + std::to_string(data.getX()) + ',' + std::to_string(data.getY()) + ',' + std::to_string(data.getZ()) + "," + std::to_string(data.getNumRoots()) + "," + std::to_string(data.getStatus()) + '\n');
         }
 
         previous_step = step_data;
 
         if (alive_tillers > 400) {
-            std::cout << "Too many alive tillers in simulation number: " << sim_id << "\n";
+            logFile << "Too many alive tillers in simulation number: " << sim_id << " at iteration: " << time_step << "\n";
+            
+            // Add a tiller at x=50 and another at x=-50
+            step_data.emplace_back(Tiller(1, 1, 0.5, 50.0, 0, 0, 3, 1));
+            step_data.emplace_back(Tiller(1, 1, 0.5, -50.0, 0, 0, 3, 1));
+            
             break;
         }
     }
 
-    std::cout << "Finished sim number: " << sim_id <<"\n";
+    std::cout << "Finished sim number: " << sim_id << "\n";
 
     std::string big_buffer = "TimeStep,Age,SizeClass,Radius,X,Y,Z,NumRoots,Status\n";
-    for(std::string& step: buffer){
+    for (std::string& step : buffer) {
         big_buffer += step;
     }
     outputFile << big_buffer;
+    logFile.close();
 }
 
 int main() {
@@ -233,7 +219,6 @@ int main() {
 
     for (int sim_id = 0; sim_id < num_sims; sim_id++) {
         std::cout << "Starting Simulation Number: " << sim_id << "\n";
-
 
         threads.emplace_back(simulate, max_sim_time, sim_id, outdir);
 
